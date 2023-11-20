c826db1b377471ac466cc41c97525ffcc6d8c87f
gabrielabiasi <gabri@DESKTOP-KJ14FRM>
2023-11-19 22:42:32 -0300

    Adicionar arquivos de projeto.

---
+++ HangmanGame/Controllers/HangmanController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HangmanGame.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HangmanController : ControllerBase
+    {
+        string words = "application";
+
+        private readonly ILogger<HangmanController> _logger;
+
+        public HangmanController(ILogger<HangmanController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpPost("new")]
+        //public IActionResult StartNewGame()
+        //{
+        //    // Lógica para iniciar um novo jogo
+
+
+        //}
+    }
+}
---
+++ HangmanGame/Model/Game.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics.Metrics;
+using System.Xml;
+
+namespace HangmanGame.Model
+{
+    public class Game
+    {
+        public static void Play(string letter)
+        {
+           
+
+
+        }
+        public string ChooseWord(string ChosenWord)
+        {
+            string xml = "./Data/words.xml";
+            int WordIndex;
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            try
+            { 
+                // Carregar o arquivo XML
+                xmlDoc.Load(xml);
+
+                // Encontrar todos os elementos 'word' dentro de 'word_list'
+                XmlNodeList? wordNodes = xmlDoc.SelectNodes("//hangman/word_list/word");
+
+                // Verificar se há palavras
+                if (wordNodes != null && wordNodes.Count > 0)
+                {
+                    // Escolher aleatoriamente uma palavra
+                    Random random = new Random();
+                    WordIndex = random.Next(wordNodes.Count);
+                    ChosenWord = wordNodes[WordIndex].InnerText;
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while reading the XML file: " + ex.Message);
+            }
+
+            return ChosenWord;
+        }
+    }
+}
